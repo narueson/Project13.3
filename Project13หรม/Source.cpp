d4f89077180af14f343077d4d346226c46d04858
narueson22 <narueson22@outlook.com>
2020-08-23 12:05:00 +0700

    Add project files.

---
+++ Project13หรม/Source.cpp
@@ -0,0 +1,26 @@
+﻿#include<stdio.h> 
+int main()
+
+{
+
+	int A, B, start;
+
+	printf("Enter two positive intergers: ");
+
+	scanf_s("%d %d", &A, &B);
+
+	if (A < B) start = A;
+
+	else start = B;
+
+	while (((A % start) != 0) || ((B % start) != 0))
+
+	{
+
+		start = start - 1;
+
+	}
+
+	printf("The g.c.d of %d and %d is %d", A, B, start);
+
+}
